dc48abff0e5c55891852d3f808f816238679dd53
ksandwojtek <65530713+ksandwojtek@users.noreply.github.com>
2022-01-26 21:05:34 +0100

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,1 +1,37 @@
+#!/usr/bin/env python
+import os
+from bot.bot import Bot
+import json
+from bot.constants import TOKEN
 
+
+
+def get_config():
+    with open("config.json") as file:
+        config = json.load(file)
+
+    return config
+
+def load_cogs():
+    print('Loading cogs...')
+    for fn in os.listdir("./bot/cogs"):
+        if not fn.startswith("__"):
+            if fn.endswith(".py"):
+                print(f"Loading {fn}...")
+                client.load_extension(f"bot.cogs.{fn[:-3]}")
+
+
+def main():
+    global client
+    global tab, categ
+    categ = "  "
+    tab =  "⠀⠀⠀ "
+
+    client = Bot()
+
+    load_cogs()
+
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
